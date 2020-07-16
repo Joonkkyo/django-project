ebd77a23192198d5570253da3be3fce520fa92bb
Junkyo <jkseo50@naver.com>
2020-07-16 16:27:46 +0900

    Login, Logout, 회원가입 기능 추가

--- ch99/blog/urls.py
+++ ch99/blog/urls.py
@@ -5,30 +5,20 @@ app_name = 'blog'
 urlpatterns = [
     #Example: /blog/
     path('', views.PostLV.as_view(), name='index'),
-
     #Example: /blog/post/
     path('post/', views.PostLV.as_view(), name='post_list'),
-
     #Example: /blog/post/django-example/
     re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
-
     #Example: /blog/archive/
     path('archive/', views.PostAV.as_view(), name='post_archive'),
-
     #Example: /blog/archive/2019
     path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
-
     #Example: /blog/archive/2019/nov/
     path('archive/<int:year>/<int:month>/', views.PostMAV.as_view(month_format='%m'), name='post_month_archive'),
-
     path('archive/<int:year>/<int:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
-
     path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
-
     path('tag/', views.TagCloudTV.as_view(), name='tag_cloud'),
-
     path('tag/<str:tag>/', views.TaggedObjectLV.as_view(), name='tagged_object_list'),
-
     #Example: /blog/search
     path('search/', views.SearchFromView.as_view(), name='search'),
 ]
