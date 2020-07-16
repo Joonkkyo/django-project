7ebe4e33e1df7f66f4b56c48bd9dc0d7d289e5a5
Junkyo <jkseo50@naver.com>
2020-07-16 15:00:38 +0900

    album 기능 추가

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from mysite.views import HomeView
+from django.conf import settings
+from django.conf.urls.static import static
 from bookmark.views import BookmarkLV, BookmarkDV
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
+    path('photo/', include('photo.urls')),
     # path('bookmark/', BookmarkLV.as_view(), name='index'),
     # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
